4823122e80b6d16b6226a464570c299c7652e347
Thang <pandothang@gmail.com>
2017-04-16 21:57:09 +0200

    Init commit

---
+++ public/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+	setTimeout(
+	  function()
+	  {
+	  	var welcomeText = $('h1')
+		welcomeText.show();
+		welcomeText.addClass('animated bounceInLeft');
+
+		console.log('nazdar')
+	  }, 800);
+})
+
